98e5ada12e94f5b9a5cc68e4ccf2fbc408741e30
nurxan02 <nurkhantm@code.edu.az>
2025-03-07 13:05:22 +0100

    front ver 1 added

--- qr_attendance_system/qr_attendance/attendance/admin.py
+++ qr_attendance_system/qr_attendance/attendance/admin.py
@@ -3,3 +3,5 @@ from .models import Employee, Attendance
 
 admin.site.register(Employee)
 admin.site.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ['employee', 'check_in', 'check_out']
